fbca7c8e1a3578dcbe11f256ae9fed89914ae44a
Elecrow-keen <keen@elecrow.com>
2017-11-01 03:50:13 +0000

    V1.0

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ git clone https://github.com/adafruit/Adafruit_Python_SSD1306
 cd ~/Adafruit_Python_SSD1306
 sudo python setup.py install
 cd ~/
-rm -rf Adafruit_Python_SSD1306
+sudo rm -rf Adafruit_Python_SSD1306
